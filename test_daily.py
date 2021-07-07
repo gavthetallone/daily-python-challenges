eef34629705511cd4e1b16c52d1629d59c86097a
gavthetallone <gavinvw123@gmail.com>
2021-07-07 14:11:47 +0000

    GW: Added tests for Monday and Tuesday

---
+++ test_daily.py
@@ -0,0 +1,9 @@
+from monday import alphasort
+from tuesday import fibonacci
+
+def test_alphasort():
+    assert alphasort("hello world and practice makes perfect and hello world again") == "\n: again and hello makes perfect practice world "
+
+def test_fibonacci():
+    assert fibonacci(5) == "\n\tf(n) = 5\n"
+    assert fibonacci(1) == "\n\tf(n) = 1\n" 
